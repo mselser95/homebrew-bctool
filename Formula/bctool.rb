d36addffe938822c7dc13a5d9ecca82ea86d9534
Matias Selser <mati.selser@hotmail.com>
2025-04-18 21:53:08 -0300

    FEAT: Add Homebrew formula for bctool
    Introduce a Homebrew formula for the bctool application, supporting macOS and Linux for both Intel and ARM architectures. This allows users to install and manage bctool easily via Homebrew. SHA256 values need to be replaced before production use.
    
    Signed-off-by: Matias Selser <mati.selser@hotmail.com>

---
+++ Formula/bctool.rb
@@ -0,0 +1,37 @@
+class Bctool < Formula
+  desc "Blockchain info tool"
+  homepage "https://github.com/mselser95/bctool"
+  version "0.2.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/mselser95/bctool/releases/download/v0.2.0/bctool_darwin_amd64"
+      sha256 "<REPLACE_WITH_SHA_FOR_DARWIN_AMD64>"
+    end
+
+    if Hardware::CPU.arm?
+      url "https://github.com/mselser95/bctool/releases/download/v0.2.0/bctool_darwin_arm64"
+      sha256 "<REPLACE_WITH_SHA_FOR_DARWIN_ARM64>"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/mselser95/bctool/releases/download/v0.2.0/bctool_linux_amd64"
+      sha256 "<REPLACE_WITH_SHA_FOR_LINUX_AMD64>"
+    end
+
+    if Hardware::CPU.arm?
+      url "https://github.com/mselser95/bctool/releases/download/v0.2.0/bctool_linux_arm64"
+      sha256 "<REPLACE_WITH_SHA_FOR_LINUX_ARM64>"
+    end
+  end
+
+  def install
+    bin.install "#{stable.url.split('/')[-1]}" => "bctool"
+  end
+
+  test do
+    system "#{bin}/bctool", "--help"
+  end
+end
